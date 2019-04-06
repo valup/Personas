cc455f97f6ffe58098c308b828e0d382de88d244
valup <44057199+valup@users.noreply.github.com>
2019-04-06 16:05:11 -0300

    Update tp1lista.c

--- tp1lista.c
+++ tp1lista.c
@@ -13,3 +13,25 @@ typedef struct _SNodo {
 } SNodo;
 
 typedef SNodo *SList;
+
+int archivo_a_glist( GList lista , char *nombre ) {
+	int i = 0;
+	FILE *archivo;
+	archivo = fopen( nombre , "r" );
+	while( !feof( archivo ) ) {
+		char *nombre;
+		fscanf( archivo , nombre );
+		lista = glist_agregar_final( lista , nombre );
+		i++;
+	}
+	return i;
+}
+
+int main() {
+	GList nombres = glist_crear()  , lugares = glist_crear()  , personas = glist_crear();
+	int cantN , cant L;
+	cantN = archivo_a_glist( nombres , "nombres.txt" );
+	cantL = archivo_a_glist( lugares , "lugares.txt" );
+	
+	return 0;
+}
